d37c01b28bcca8aad36ed6755ec10fe2d82a9ac7
muftauridwan1999 <muftauridwan99@gmail.com>
2022-06-03 23:41:15 -0700

    wow

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - ENtry point
- * 
- * REturn: Always 0 (Success)
- */
-int main (void)
- {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
- }
+* main -Entry point
+*
+* Return always 0 (sucess)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
