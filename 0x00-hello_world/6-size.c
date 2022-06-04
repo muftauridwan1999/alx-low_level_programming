9624bc284e0ce6e9dadb213249df9c4b654f5143
muftauridwan1999 <muftauridwan99@gmail.com>
2022-06-04 06:46:27 -0700

    me

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,12 @@ int main(void)
 {
 char c;
 int i;
-double d;
-double e;
+long int d;
+long long int e;
 float f;
 printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu.\n", (signed long)sizeof(d));
+printf("Size of a long int: %lu.\n", (unsigned long)sizeof(d));
 printf("Size of long long int: %lu.\n", (unsigned long)sizeof(e));
 printf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
 }
