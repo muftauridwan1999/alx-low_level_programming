59605913e761c8f4dd617a31d4cf7147c4695f18
muftauridwan1999 <muftauridwan99@gmail.com>
2022-06-08 10:16:24 -0700

    i hae been so cool

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - check the code
+*_abs - finding absolute value
+*Return: always 0.
+*/
+int _abs(int c)
+{
+if (c < 0)
+{
+int m = ((-1) * c);
+printf("%d\n", m); 
+}
+else if ( c == 0)
+{
+printf("%d\n", c);
+}
+else
+{
+printf("%d\n", c);
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*print_last_digit - to printing the last digit
+*Return: ALways 0.
+*/
+int print_last_digit(int b)
+{
+_putchar('0' + (b % 10));
+return (0);
+}
