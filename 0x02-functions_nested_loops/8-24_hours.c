b55a0169cf7d6a0f5932c0991846a598ecdcddc2
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-09 04:07:40 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * jack_bauer -prints every minute of the day
+ *
+ * Return: Always 0.
+ */
 void jack_bauer(void)
 {
 int t;
@@ -6,17 +11,17 @@ int g;
 t = 0;
 while (t < 24)
 {
- g = 0;
+g = 0;
 while ( g < 60)
 {
-_putchar(t /10 + '0');
+_putchar(t / 10 + '0');
 _putchar(t % 10 + '0');
- _putchar(':');
+_putchar(':');
 _putchar(g / 10 + '0' );
 _putchar(g % 10 + '0');
 _putchar('\n');
- g++;
+g++;
 }
- t++;
+t++;
 }
 }
