43821b5bd76911d1882bd264c6a37c143b6bcf4b
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-09 04:09:20 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,12 @@ t = 0;
 while (t < 24)
 {
 g = 0;
-while ( g < 60)
+while (g < 60)
 {
 _putchar(t / 10 + '0');
 _putchar(t % 10 + '0');
 _putchar(':');
-_putchar(g / 10 + '0' );
+_putchar(g / 10 + '0');
 _putchar(g % 10 + '0');
 _putchar('\n');
 g++;
