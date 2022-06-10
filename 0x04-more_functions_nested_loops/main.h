4733f9a6127f31e3b89e52c7a83224da5f4c2a57
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-10 14:58:20 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,7 +7,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-_putchar (char c)
+_putchar (char c);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
