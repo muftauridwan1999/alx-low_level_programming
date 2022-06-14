9367e08266a37d3fe370625aef25f4fe48d6ae0e
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-14 12:32:13 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+*print_rev- printing reverse orders
+*@s: the string
+*
+*/
 void print_rev(char *s)
 {
 int x = 0;
@@ -6,7 +11,7 @@ while (s[x] != '\0')
 
 	x++;
 x--;
-while (x >= 0 )
+while (x >= 0)
 {
 _putchar(s[x]);
 x--;
