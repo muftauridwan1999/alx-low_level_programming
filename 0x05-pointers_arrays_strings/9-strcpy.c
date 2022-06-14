decf2e470f01a1e28476a873f96ef3b14d1ce260
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-14 12:48:41 -0700

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,17 @@
 #include "main.h"
+/**
+*_strcpy - copy string
+*@src: wow
+*@dest: me
+*Return: success
+*/
 char *_strcpy(char *dest, char *src)
 {
- int z;
- for (z = 0; src[z] != '\0'; z++)
-      {
-       dest[z] = src[z];
-      }
-   dest[z] = '\0';
-     return (dest);
-  
+int z;
+for (z = 0; src[z] != '\0'; z++)
+{
+dest[z] = src[z];
+}
+dest[z] = '\0';
+return (dest);
 }
