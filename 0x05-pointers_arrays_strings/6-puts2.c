bb65ef25b23b07fb3ab17dad61b3ea0254b3f401
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-14 12:37:14 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,19 @@
 #include "main.h"
+/**
+*puts2 - string printing
+*@str: the string
+*
+*/
 void puts2(char *str)
 {
-int p=0;
+int p = 0;
 while (str[p] != '\0')
 
 {
-	if ( p % 2 == 0)
+	if (p % 2 == 0)
 
 {
-	_putchar(str[p]);        
+	_putchar(str[p]);    
 }
 p++;
 }
