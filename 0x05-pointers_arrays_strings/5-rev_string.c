8e1a1ab5afb7ed2aab3a8445f81ad26cd2bf3a1b
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-14 12:34:09 -0700

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+*rev_string - reverse string
+*@s: the string
+*
+*/
 #include <stdio.h>
 void rev_string(char *s)
 {
@@ -8,7 +13,7 @@ char ng;
 while (s[st] != '\0')
 st++;
 st--;
-while ( ri < st )
+while (ri < st)
 {
 ng = s[st];
 s[st] = s [ri];
