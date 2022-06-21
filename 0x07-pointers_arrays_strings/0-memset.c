c0f5c6982186ebfea99815cbf31c961e22026134
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-21 09:23:07 +0100

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include main.h
+#include <stdio.h>
+/**
+*_memset - setting a constant memory variable
+*@s: string to be set constant
+*@b: the constant string
+*@n: at n string
+*Return: string s
+*/
+char *_memset(char *s, char b, unsigned int n);
+{
+unsigned int m;
+for(m = 0; m < n; m++)
+{
+s[m] = b;
+}
+return (s);
+}
