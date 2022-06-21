86b8de55130fcf54f124d3acfd98594d9c5a3277
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-21 10:54:03 +0100

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_strbrk - searching for string in byte
+*@s: first string
+*@accept: second
+*@return: c or null
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int m;
+while (*s)
+{
+m = 0
+while (accept[m])
+{
+if (*s == accept[m])
+return (s);
+m++;
+}
+s++;
+}
+return (NULL);
+}
