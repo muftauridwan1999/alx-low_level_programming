58218850d985f50044b8643a416f9ccc87374022
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-21 10:33:54 +0100

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_strspn - string length
+*@s: first string
+*@accept: second string
+*Return: return c
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+int m,n;
+int i = 0;
+for (m = 0; s[m] != '\0'; m++)
+{
+if (s[m] != 32)
+{
+for (n = 0; accept[n] != '\0'; n++)
+{
+if (s[m] == accept[n])
+i++;
+}
+}
+else
+return (i);
+}
+return (i);
+}
