94b64d3f542cbd893577cfcf7da1da2bf8a570ec
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-21 09:33:23 +0100

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_memcpy - memory coping function
+*@dest: first string
+*@src: second string
+*@n: bytes
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int m;
+for (m = 0; m < n; m++)
+{
+dest[m] = src [m];
+return (dest);
+}
+}
