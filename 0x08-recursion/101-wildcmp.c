2bb31ef771a3958315ae32ffc9cb764f303976ca
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-24 00:32:36 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,11 +7,9 @@
 */
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
-{
-return (1);
-}
-else if (*s2 == '*')
+if (*s2 == '*')
+{return (1); }
+else if (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 {
 return (1);
 }
