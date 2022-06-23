5485a9dbd2dd7d1a50cd18d5c405bd17efb42fda
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-24 00:12:33 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,13 @@
 #include "main.h"
+/**
+*wildcmp - comparison
+*@s1: string 1
+*@s2: string two
+*Return: 1 or 0
+*/
 int wildcmp(char *s1, char *s2)
 {
- if (*s1 != '\0' && *s2 != '\0' && *s1 ==*s2)
+if (*s1 != '\0' && *s2 != '\0' && *s1 ==*s2)
 {
 return (1);
 }
