d30ec665159d7646cbdb42e004e5a5c9a3c98a19
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-22 18:28:35 -0700

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,20 @@
 #include "main.h"
+/**
+*is_prime_number - prime numbers
+*@n: the prime number to be checked
+*@Return: return value conditions
 int is_prime_number(int n)
 {
-  if (n % 2 == 1)
-  {
-    return (1);
-  }
-  else if (n % 2 > 1)
-  {
-    return (0);
-  }
-  else
-  {
-    return (0);
-  } 
+if (n % 2 == 1)
+{
+return (1);
+}
+else if (n % 2 > 1)
+{
+return (0);
+}
+else
+{
+return (0);
+} 
 }
