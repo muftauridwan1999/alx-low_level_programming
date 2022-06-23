742a92ee388fd440e4bd2b8c1bcdc102c336a647
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-22 18:11:26 -0700

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+*_print_rev_recursion - printing a string content in everse
+*@s: the string to print it content in reversed
+*/
 void _print_rev_recursion(char *s)
 {
   if ( *s != '\0')
