053477cc710b5e09bab546f0a3badb46f5c2b685
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-25 15:40:23 -0700

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-int main (int argc , char *argv[])
+int main(int argc, char *argv[])
 {
 argc = argc;
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
