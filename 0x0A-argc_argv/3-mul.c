2bed53997440200e7f6fe3178e8e23dae484bd6d
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-25 16:45:48 -0700

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,15 +5,15 @@ int main (int argc, char *argv[])
   int i,c, mul;
   if (argc != 3)
   {
-    printf("Error\n")
+    printf("Error\n");
       return (1);
   }
   else
   {
-   i = atoi(argv[1]):
+   i = atoi(argv[1]);
    c = atoi(argv[2]);
    mul = i * c;
     printf("%d\n", mul);
- return (0);
   }
+  return (0);
 }
