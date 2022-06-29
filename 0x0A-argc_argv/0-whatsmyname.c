07f6b7144299f7aacf298b989c147aa5d0490baf
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-06-29 09:03:04 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,12 @@
 *main - the prototype 
 *@argc: argument count only
 *@argv: argument vector
+*Return: 0
 */
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
 int i;
-for ( i = 0; i < argc; i++)
+for (i = 0; i < argc; i++)
 printf("%s\n", argv[i]);
 return (0);
 }
